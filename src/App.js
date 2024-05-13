189b2c0cc7096308f347702883efdd087959e78d
Vedant <vgawande08@gmail.com>
2024-05-13 15:42:53 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { ResumeProvider } from './Context';
+import './App.css';
+import Header from './components/Layouts/Header';
+import Navbar from './components/Layouts/Navbar';
+import Main from './components/Main';
+import WebFont from 'webfontloader';
+
+function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Pacifico', 'Poppins']
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <ResumeProvider>
+        <Navbar />
+        <Header />
+        <Main />
+      </ResumeProvider>
+    </>
+  );
+}
+
+export default App;
